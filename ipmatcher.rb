f59ea39a041e5ca065bdb313fbbd02e3813dc798
Dirk Haage <dirk@caffeinatedcoders.com>
2011-02-15 16:35:06 +0100

    ipmatcher using sqlite

--- ipmatcher.rb
+++ ipmatcher.rb
@@ -1,38 +1,88 @@
 require 'rubygems'
-require 'redis'
+require 'sqlite3'
 
-rhost = "localhost"
-rport = "6379"
 
-def get_prefix_start(start,stop)
-  return start & stop
+
+module Ipmatcher
+  class MaxMindMatcher
+    include SQLite3
+    
+    def initialize(db, blocks = nil, locations = nil)
+      @blocks = blocks
+      @locations = locations
+      @db = Database.new(db)
+    end
+    
+    def update()
+      c = 0
+      begin
+        File.open(@blocks).each{ |line|
+          # delete & prepare table
+          @db.execute("drop table if exists blocks")
+          rows = @db.execute <<-SQL
+              create table blocks (
+                start int,
+                stop int,
+                location int
+              );
+            SQL
+          # read maxmind file
+          data = line.chomp.split(',')
+          data.each{ |i|
+            i.gsub!(/"/,'')
+          }
+          start = data[0].to_i
+          stop = data[1].to_i
+          loc = data[2].to_i
+          # put into db
+          @db.execute("insert into blocks values (?,?,?)", start, stop, loc)
+          c += 1
+          if (c%1000) == 0
+            puts "inserted " + c.to_s + " blocks."
+          end
+        }
+      rescue Exception => e
+        puts e
+        puts "No blocks file provided, not updating!"
+      end
+      puts "Finished. inserted " + c.to_s + " blocks."
+      c = 0
+      begin
+        File.open(@locations).each{ |line|
+          # delete & prepare table
+           @db.execute("drop table if exists locations")
+          rows = @db.execute <<-SQL
+              create table locations (
+                location int,
+                lat real,
+                lon real
+              );
+            SQL
+          # read maxmind file
+          data = line.chomp.split(',')
+          data.each{ |i|
+            i.gsub!(/"/,'')
+          }
+          location = data[0].to_i
+          lat = data[5].to_f
+          lon = data[6].to_f
+          @db.execute("insert into locations values (?,?,?)", location, lat, lon)
+          c += 1
+          if (c%1000) == 0
+            puts "inserted " + c.to_s + " locations."
+          end
+        }
+      rescue Exception => e
+        puts e
+        puts "No location file provided, not updating"
+      end
+      puts "Finished. inserted " + c.to_s + " locations."
+    end
+  end
 end
 
-def get_prefix_length(start,stop)
-    (Math.log(stop-start+1)/Math.log(2)).ceil 
-end
+m = Ipmatcher::MaxMindMatcher.new("maxmind.db",
+                                  "data/GeoLite_20110101/GeoLiteCity-Blocks.csv",
+                                  "data/GeoLite_20110101/GeoLiteCity-Location.csv")
+m.update()
 
-def get_prefix(start, stop)
-  s = get_prefix_start(start, stop)
-  l = get_prefix_length(start, stop)
-  return s, l
-end
-
-# Initialize the connection to Redis
-begin 
-  r = Redis.new(:host => rhost, :port => rport)
-rescue 
-  abort("Redis could not connect")
-end
-
-File.open("data/GeoLite_20110101/GeoLiteCity-Blocks.csv").each{ |line|
-  data = line.chomp.split(',')
-  data.each{ |i|
-    i.gsub!(/"/,'')
-  }
-  start = data[0].to_i
-  stop = data[1].to_i
-  loc = data[2]
-  s,l = get_prefix(start, stop)
-  puts s.to_s(2)[0..-l]
-}
