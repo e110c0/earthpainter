6b20d5e67d1cc51256d477c1087088825921e397
Dirk Haage <dirk@caffeinatedcoders.com>
2011-02-17 11:48:16 +0100

    sqlite bugfixes implemented get_coordinates

--- ipmatcher.rb
+++ ipmatcher.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'sqlite3'
-
-
+require 'ipaddr'
 
 module Ipmatcher
   class MaxMindMatcher
@@ -15,17 +14,16 @@ module Ipmatcher
     
     def update()
       c = 0
-      begin
+      if @blocks != nil
+        @db.execute("drop table if exists blocks")
+        rows = @db.execute <<-SQL
+            create table blocks (
+              start int,
+              stop int,
+              location int
+            );
+          SQL
         File.open(@blocks).each{ |line|
-          # delete & prepare table
-          @db.execute("drop table if exists blocks")
-          rows = @db.execute <<-SQL
-              create table blocks (
-                start int,
-                stop int,
-                location int
-              );
-            SQL
           # read maxmind file
           data = line.chomp.split(',')
           data.each{ |i|
@@ -41,23 +39,22 @@ module Ipmatcher
             puts "inserted " + c.to_s + " blocks."
           end
         }
-      rescue Exception => e
-        puts e
+      elsif
         puts "No blocks file provided, not updating!"
       end
       puts "Finished. inserted " + c.to_s + " blocks."
       c = 0
-      begin
+      if @locations != nil
+        # delete & prepare table
+         @db.execute("drop table if exists locations")
+        rows = @db.execute <<-SQL
+            create table locations (
+              location int,
+              lat real,
+              lon real
+            );
+          SQL
         File.open(@locations).each{ |line|
-          # delete & prepare table
-           @db.execute("drop table if exists locations")
-          rows = @db.execute <<-SQL
-              create table locations (
-                location int,
-                lat real,
-                lon real
-              );
-            SQL
           # read maxmind file
           data = line.chomp.split(',')
           data.each{ |i|
@@ -72,12 +69,26 @@ module Ipmatcher
             puts "inserted " + c.to_s + " locations."
           end
         }
-      rescue Exception => e
-        puts e
+      elsif
         puts "No location file provided, not updating"
       end
       puts "Finished. inserted " + c.to_s + " locations."
     end
+
+    def get_coordinates(ip)
+      if ip.class == String:
+        ip = IPAddr.new(ip).to_i
+      elsif ip.class == Fixnum:
+        # do nothing
+      else
+        puts "unknown IP type!"
+        return nil
+      end
+      return @db.execute("SELECT location from blocks where (start <= ?) AND (stop >= ?) LIMIT 1", ip, ip)
+      
+    end
+    
+    
   end
 end
 
@@ -85,4 +96,17 @@ m = Ipmatcher::MaxMindMatcher.new("maxmind.db")#,
                                   #"data/GeoLite_20110101/GeoLiteCity-Blocks.csv",
                                   #"data/GeoLite_20110101/GeoLiteCity-Location.csv")
 m.update()
-
+before = Time.new
+(20...30).each{ |a|
+  (30...40).each { |b|
+    ip = a.to_s + "." + b.to_s + ".34.56"
+    location = m.get_coordinates(ip)
+    begin
+      puts ip + ": " + location.to_s
+    rescue Exception => e
+      puts ip + ": none"
+    end
+  }
+}
+after = Time.new
+puts "100 request in %f seconds"  % (after-before)
